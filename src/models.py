6436acef3c1dc57dd3c7fbaa36c13dbeaec7ddfc
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-08-30 12:38:37 +0000

    structure_enhanced_with_new_tables_added

--- src/models.py
+++ src/models.py
@@ -1,92 +1,70 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, ForeignKey, Table, enumerate
-from sqlalchemy.orm import relationship, backref, declarative_base, sessionmaker
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Table
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///instagram.db')
-Session = sessionmaker(bind=engine)
-session = Session()
-
-user_account_table = Table('user_id', Base.metadata,
-                             Column('user_from_id', Integer, ForeignKey('users.id')),
-                             Column('media_id', Integer, ForeignKey('media.id')))
-
-class Follower(Base):
-    __tablename__ = 'followers'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    user_from_id = Column(Integer)
-    user_to_id = Column(Integer)
-
-user = relationship("users",
-                            secondary=user_account_table,
-                            back_populates="followers")
-
 class User(Base):
     __tablename__ = 'users'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True, ForeignKey=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(250), unique=True, nullable=False)
     firstname = Column(String(250))
     lastname = Column(String(250))
-    email = Column(String(250), unique=True, nullable=False)     
+    email = Column(String(250), unique=True, nullable=False)
 
+class Follower(Base):
+    __tablename__ = 'followers'
+    user_from_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
 
-class Media(Post):
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
+class MediaType(Enum):
+    image = "image"
+    video = "video"
+
+class Media(Base):
     __tablename__ = 'media'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True, ForeignKey=True)
-    type = Column(enumerate)
+    id = Column(Integer, primary_key=True)
+    type = Column(MediaType, nullable=False)
     url = Column(String(250))
-    post_id = Column(Integer)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'media',
-        'polymorphic_on': type
-    }
-
-    users = relationship("User", 
-                        secondary=user_account_table,
-                        back_populates="media")
-
-class Post(Post):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True, ForeignKey=True)
-    user_id = Column(Integer)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'post',
-    }
-
-class Comment(Post):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True, ForeignKey=True)
-    comment_text = Column(String)
-    author_id = Column(id)
-    post_id = Column(Integer)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'comment',
-    }
+    post_id = Column(Integer, ForeignKey('posts.id'))
 
     def to_dict(self):
-        return {}
+        return {
+            "id": self.id,
+            "type": self.type.value,
+            "url": self.url,
+            "post_id": self.post_id
+        }
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
-
+engine = create_engine('sqlite:///instagram.db', echo=True)  # Set echo to False to suppress SQLAlchemy logs
 Base.metadata.create_all(engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Create a new user
 new_user = User(username="@salomonfranco", email="salo@gmail.com")
-
 session.add(new_user)
-
 session.commit()
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem generating the diagram")
+    raise e
